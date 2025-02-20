9a2c683a28b4d5fbd5dc4989eafe00d977bf97b5
Ivanctga <ivanctga@gmail.com>
2025-02-20 09:06:07 -0300

    1 º commit

---
+++ between.sql
@@ -0,0 +1,48 @@
+
+        -- * BETWEEN  --
+
+    -- ? O between é usado para encontrar valor entre um valor inicial e um valor final.
+    -- * Valor BETWEEN mínimo AND máximo;
+    -- * É a mesma coisa que dizer : 
+    --!Valor >= minimo AND Valor <= maximo;
+
+
+SELECT coluna1, coluna2
+FROM TABELA 
+WHERE coluna1 BETWEEN 1 AND 10
+
+SELECT *
+FROM Production.Product
+WHERE ListPrice NOT between 1000 and 1500;
+
+SELECT *
+FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' and '2010/01/01' 
+order by HireDate
+
+
+/*
+
+    -- * DESAFIO 1 --
+    *    
+
+*/
+
+
+
+/*
+
+    -- * DESAFIO 2 --
+    *    
+
+*/
+
+
+
+/*
+
+    -- * DESAFIO 3 --
+    *    
+
+*/
+
---
+++ in.sql
@@ -0,0 +1,40 @@
+
+        -- * IN  --
+
+    -- ? Nós usamos o perador IN junto com WHERE, para verificar se o valor correspondente ao campo especificado existe em uma lista de valores.
+    -- ! Valor IN (valore1, valore2);
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID IN (2,7,13)
+
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID NOT IN (2,7,13)
+
+
+/*
+
+    -- * DESAFIO 1 --
+    *    
+
+*/
+
+
+
+/*
+
+    -- * DESAFIO 2 --
+    *    
+
+*/
+
+
+
+/*
+
+    -- * DESAFIO 3 --
+    *    
+
+*/
